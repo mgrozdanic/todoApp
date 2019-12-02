c6f681b37d3df479fce01d3b4e89afc02b7af5ec
Milan Grozdanic <mgrozdanic333@gmail.com>
2019-12-02 15:21:46 +0100

    CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,6 +11,8 @@ const bodyParser = require('body-parser');
 const expressJwt = require('express-jwt');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
