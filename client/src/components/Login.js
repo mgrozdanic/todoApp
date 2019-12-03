e88926a0948ecc77a3dbae46db8e031ef981c6cf
Milan Grozdanic <mgrozdanic333@gmail.com>
2019-12-03 15:59:24 +0100

    Login fix

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,8 +9,12 @@ function Login(){
     
     const login = async() => {
         let res = await(axios.post('http://localhost:9000/login', {username, password}));
-        localStorage.setItem('token', JSON.stringify(res.data));
-        setUser(res.data);
+        if (res.data !== 'Bad credentials.'){
+            localStorage.setItem('token', JSON.stringify(res.data));
+            setUser(res.data);
+        } else {
+            alert(res.data);
+        }
     }
     const handleSubmit = (e) => {
         e.preventDefault();
